be88640b3ccdbe65f9c565d19329d58083a9236f
Shashi Sekhar Jaiswal <ssekharj11@gmail.com>
2020-06-29 04:07:21 +0530

    start script

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()  ;
 const User = require ('../model/User');
-const { restart } = require('nodemon');
+//const { restart } = require('nodemon');
 const jwt = require ('jsonwebtoken') ;
 
 //Register 
